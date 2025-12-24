ad1e4a8ec5c38c7c1c67eb443146d48f1a1b3827
张传辉 <414768208@qq.com>
2025-12-24 08:25:21 +0800

    fix: 优化email检查

--- packages/form/validate.ts
+++ packages/form/validate.ts
@@ -50,7 +50,7 @@ export const validateGlobalFunction = {
         return (value: any) => {
             if (!value) return;
             if (
-                /^([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$/.test(
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\.[a-zA-Z]{2,}$/.test(
                     value
                 ) === false
             ) {
